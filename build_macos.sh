3986255ee1355080e7f05f712405c8edb0b07121
zauonlok <zauonlok@gmail.com>
2017-05-30 22:10:11 +0800

    Move buffer.c into model.c

--- build_macos.sh
+++ build_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPTS="-o viewer.out -std=c89 -Wall"
-SRCS="main.c platform_macos.m image.c buffer.c geometry.c model.c graphics.c"
+SRCS="main.c platform_macos.m image.c geometry.c model.c graphics.c"
 LIBS="-framework Cocoa"
 
 clang $OPTS $SRCS $LIBS
--- model.c
+++ model.c
@@ -4,8 +4,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include "model.h"
-#include "buffer.h"
 #include "geometry.h"
+
+/* dynamic array */
+
+#define BUFFER_RAW_DATA(buffer) ((int*)(buffer) - 2)
+#define BUFFER_CAPACITY(buffer) (BUFFER_RAW_DATA(buffer)[0])
+#define BUFFER_OCCUPIED(buffer) (BUFFER_RAW_DATA(buffer)[1])
+
+#define buffer_hold(buffer, count)                                          \
+    do {                                                                    \
+        (buffer) = buffer_hold_helper(buffer, count, sizeof(*(buffer)));    \
+    } while (0)
+
+#define buffer_push(buffer, value)                                          \
+    do {                                                                    \
+        buffer_hold(buffer, 1);                                             \
+        (buffer)[buffer_size(buffer) - 1] = (value);                        \
+    } while (0)
+
+static void buffer_free(void *buffer) {
+    if (buffer != NULL) {
+        free(BUFFER_RAW_DATA(buffer));
+    }
+}
+
+static int buffer_size(void *buffer) {
+    return buffer != NULL ? BUFFER_OCCUPIED(buffer) : 0;
+}
+
+static void *buffer_hold_helper(void *buffer, int count, int itemsize) {
+    if (buffer == NULL) {
+        int *base = (int*)malloc(sizeof(int) * 2 + itemsize * count);
+        base[0] = count;  /* capacity */
+        base[1] = count;  /* occupied */
+        return base + 2;
+    } else if (BUFFER_OCCUPIED(buffer) + count <= BUFFER_CAPACITY(buffer)) {
+        BUFFER_OCCUPIED(buffer) += count;
+        return buffer;
+    } else {
+        int needed_size = BUFFER_OCCUPIED(buffer) + count;
+        int double_curr = BUFFER_CAPACITY(buffer) * 2;
+        int capacity = needed_size > double_curr ? needed_size : double_curr;
+        int occupied = needed_size;
+        int *base = (int*)realloc(BUFFER_RAW_DATA(buffer),
+                                  sizeof(int) * 2 + itemsize * capacity);
+        base[0] = capacity;
+        base[1] = occupied;
+        return base + 2;
+    }
+}
+
+/* model stuff */
 
 struct model {
     vec3f_t *vertices;
