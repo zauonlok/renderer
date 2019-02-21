63468000a9073bf0e06d1a0585f7da641cf6a185
zauonlok <zauonlok@gmail.com>
2019-02-21 18:13:21 +0800

    Undo spark image resizing
    Former-commit-id: 1c86da8f557ab672106c21d4240c99e1a123f865

--- scripts/craftsman.py
+++ scripts/craftsman.py
@@ -74,11 +74,12 @@ def process_images(zip_file):
     for old_filename, tga_filename in IMG_FILENAMES.items():
         with zip_file.open(old_filename) as f:
             image = Image.open(f)
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
             if tga_filename == "smith_diffuse.tga":
                 bands = image.split()
                 image = Image.merge("RGB", bands[:3])
-            image = image.transpose(Image.FLIP_TOP_BOTTOM)
-            image = image.resize((512, 512), Image.LANCZOS)
+            if "spark_" not in tga_filename:
+                image = image.resize((512, 512), Image.LANCZOS)
             filepath = os.path.join(DST_DIRECTORY, tga_filename)
             image.save(filepath, rle=True)
 
