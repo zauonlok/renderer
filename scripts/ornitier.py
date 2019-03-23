4fa630fdeca840eb0b04b25cfa5d6e4180765935
zauonlok <zauonlok@gmail.com>
2019-03-23 12:10:51 +0800

    Update ornitier resources
    Former-commit-id: 4920c7b70db3edf9a948a281ef5e9aa5fddd2c91

--- scripts/ornitier.py
+++ scripts/ornitier.py
@@ -3,8 +3,8 @@
 The model is available for download from
     https://sketchfab.com/models/5edb4f4c8a4b47d2a735807f0f4646cc
 
-The Python Imaging Library and NumPy are required
-    pip install pillow numpy
+The Python Imaging Library is required
+    pip install pillow
 """
 
 from __future__ import print_function
@@ -13,7 +13,6 @@ import json
 import os
 import zipfile
 from PIL import Image
-import numpy
 from utils.gltf import dump_obj_data
 
 SRC_FILENAME = "vivi_ornitier.zip"
@@ -29,34 +28,40 @@ OBJ_FILENAMES = [
     "base.obj",
 ]
 
-EMISSIVE_FILENAMES = {
-    "textures/Base_emissive.png": "base_emission.tga",
-    "textures/Body.001_emissive.png": "body_emission.tga",
-}
-
-METALLIC_FILENAMES = {
+IMG_FILENAMES = {
     "base": [
         "textures/Base_baseColor.png",
+        "textures/Base_emissive.png",
         "textures/Base_metallicRoughness.png",
     ],
     "body": [
         "textures/Body.001_baseColor.png",
-        "textures/Body.001_metallicRoughness.png",
+        "textures/Body.001_emissive.png",
+        None,
     ],
     "coat": [
         "textures/Coat.001_baseColor.png",
+        None,
         "textures/Coat.001_metallicRoughness.png",
+    ],
+    "feuga": [
+        "textures/Feuga_baseColor.png",
+        None,
+        None,
     ],
     "hands": [
         "textures/HandsRod.001_baseColor.png",
+        None,
         "textures/HandsRod.001_metallicRoughness.png",
     ],
     "hat": [
         "textures/Hat.001_baseColor.png",
+        None,
         "textures/Hat.001_metallicRoughness.png",
     ],
     "legs": [
         "textures/Legs.001_baseColor.png",
+        None,
         "textures/Legs.001_metallicRoughness.png",
     ],
 }
@@ -77,59 +82,37 @@ def load_image(zip_file, filename):
     with zip_file.open(filename) as f:
         image = Image.open(f)
         image = image.transpose(Image.FLIP_TOP_BOTTOM)
-        image = image.resize((512, 512), Image.LANCZOS)
         return image
 
 
 def save_image(image, filename):
+    image = image.resize((512, 512), Image.LANCZOS)
     filepath = os.path.join(DST_DIRECTORY, filename)
     image.save(filepath, rle=True)
 
 
-def process_feuga_images(zip_file):
-    basecolor_image = load_image(zip_file, "textures/Feuga_baseColor.png")
-    _, _, _, alpha = basecolor_image.split()
-    albedo = Image.new("L", (512, 512), color=160)
-    diffuse_image = Image.merge("RGBA", [albedo, albedo, albedo, alpha])
-    save_image(diffuse_image, "feuga_diffuse.tga")
+def process_images(zip_file):
+    for name, paths in IMG_FILENAMES.items():
+        basecolor_path, emission_path, packed_path = paths
+
+        if basecolor_path:
+            basecolor_image = load_image(zip_file, basecolor_path)
+            save_image(basecolor_image, "{}_basecolor.tga".format(name))
+
+        if emission_path:
+            emission_image = load_image(zip_file, emission_path)
+            save_image(emission_image, "{}_emission.tga".format(name))
+
+        if packed_path:
+            packed_image = load_image(zip_file, packed_path)
+            _, roughness_image, metalness_image = packed_image.split()
+            save_image(roughness_image, "{}_roughness.tga".format(name))
+            save_image(metalness_image, "{}_metalness.tga".format(name))
 
     linear_emission = [1.000, 0.553, 0.072]
     srgb_emission = [int(pow(x, 1 / 2.2) * 255) for x in linear_emission]
     emission_image = Image.new("RGB", (512, 512), color=tuple(srgb_emission))
     save_image(emission_image, "feuga_emission.tga")
-
-
-def process_emissive_images(zip_file):
-    for old_filename, tga_filename in EMISSIVE_FILENAMES.items():
-        image = load_image(zip_file, old_filename)
-        save_image(image, tga_filename)
-
-
-def process_metallic_images(zip_file):
-    for name, (basecolor_path, metallic_path) in METALLIC_FILENAMES.items():
-        basecolor_image = load_image(zip_file, basecolor_path)
-        metallic_image = load_image(zip_file, metallic_path)
-        _, _, metallic_image = metallic_image.split()
-
-        basecolor_array = numpy.array(basecolor_image) / 255.0
-        metallic_array = (numpy.array(metallic_image) / 255.0)[:, :, None]
-
-        diffuse_array = basecolor_array * (1 - metallic_array)
-        specular_array = basecolor_array * metallic_array
-
-        diffuse_image = Image.fromarray(numpy.uint8(diffuse_array * 255.0))
-        specular_image = Image.fromarray(numpy.uint8(specular_array * 255.0))
-
-        diffuse_filename = "{}_diffuse.tga".format(name)
-        specular_filename = "{}_specular.tga".format(name)
-        save_image(diffuse_image, diffuse_filename)
-        save_image(specular_image, specular_filename)
-
-
-def process_images(zip_file):
-    process_feuga_images(zip_file)
-    process_emissive_images(zip_file)
-    process_metallic_images(zip_file)
 
 
 def main():
