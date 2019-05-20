5980fdf4de9befdcce0772b68683dcfcf7e61547
zauonlok <zauonlok@gmail.com>
2019-05-20 23:44:13 +0800

    Minor changes
    Former-commit-id: 63d6a3b7463c0d2d2a72004bff70626945f440a6

--- renderer/core/macro.h
+++ renderer/core/macro.h
@@ -2,10 +2,10 @@
 #define MACRO_H
 
 #define EPSILON 1e-6f
-#define PI 3.141592653589793f
+#define PI 3.1415927f
 
-#define TO_RADIANS(degrees) ((PI / 180) * degrees)
-#define TO_DEGREES(radians) ((180 / PI) * radians)
+#define TO_RADIANS(degrees) ((PI / 180) * (degrees))
+#define TO_DEGREES(radians) ((180 / PI) * (radians))
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
 
