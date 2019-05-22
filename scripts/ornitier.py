d183eb624dd962f648a20eb364c6c971117df8af
zauonlok <zauonlok@gmail.com>
2019-05-22 23:59:47 +0800

    Add normalmap to the ornitier model
    Former-commit-id: 9341eef97bb76b6d90b57f8fbc5919d15a29855e

--- scripts/ornitier.py
+++ scripts/ornitier.py
@@ -33,38 +33,45 @@ OBJ_FILENAMES = [
 IMG_FILENAMES = {
     "base": [
         "textures/Base_baseColor.png",
-        "textures/Base_emissive.png",
         "textures/Base_metallicRoughness.png",
+        "textures/Base_normal.png",
+        "textures/Base_emissive.png",
     ],
     "body": [
         "textures/Body.001_baseColor.png",
-        "textures/Body.001_emissive.png",
         None,
+        None,
+        "textures/Body.001_emissive.png",
     ],
     "coat": [
         "textures/Coat.001_baseColor.png",
-        None,
         "textures/Coat.001_metallicRoughness.png",
+        "textures/Coat.001_normal.png",
+        None,
     ],
     "feuga": [
         "textures/Feuga_baseColor.png",
         None,
         None,
+        None,
     ],
     "hands": [
         "textures/HandsRod.001_baseColor.png",
-        None,
         "textures/HandsRod.001_metallicRoughness.png",
+        "textures/HandsRod.001_normal.png",
+        None,
     ],
     "hat": [
         "textures/Hat.001_baseColor.png",
-        None,
         "textures/Hat.001_metallicRoughness.png",
+        "textures/Hat.001_normal.png",
+        None,
     ],
     "legs": [
         "textures/Legs.001_baseColor.png",
-        None,
         "textures/Legs.001_metallicRoughness.png",
+        "textures/Legs.001_normal.png",
+        None,
     ],
 }
 
@@ -74,7 +81,7 @@ def process_meshes(zip_file):
     buffer = zip_file.read("scene.bin")
 
     for mesh_index, filename in enumerate(OBJ_FILENAMES):
-        obj_data = dump_obj_data(gltf, buffer, mesh_index)
+        obj_data = dump_obj_data(gltf, buffer, mesh_index, with_tangent=True)
         filepath = os.path.join(DST_DIRECTORY, filename)
         with open(filepath, "w") as f:
             f.write(obj_data)
@@ -95,21 +102,25 @@ def save_image(image, filename):
 
 def process_images(zip_file):
     for name, paths in IMG_FILENAMES.items():
-        basecolor_path, emission_path, packed_path = paths
+        basecolor_path, packed_path, normal_path, emission_path = paths
 
         if basecolor_path:
             basecolor_image = load_image(zip_file, basecolor_path)
             save_image(basecolor_image, "{}_basecolor.tga".format(name))
-
-        if emission_path:
-            emission_image = load_image(zip_file, emission_path)
-            save_image(emission_image, "{}_emission.tga".format(name))
 
         if packed_path:
             packed_image = load_image(zip_file, packed_path)
             _, roughness_image, metalness_image = packed_image.split()
             save_image(roughness_image, "{}_roughness.tga".format(name))
             save_image(metalness_image, "{}_metalness.tga".format(name))
+
+        if normal_path:
+            normal_image = load_image(zip_file, normal_path)
+            save_image(normal_image, "{}_normal.tga".format(name))
+
+        if emission_path:
+            emission_image = load_image(zip_file, emission_path)
+            save_image(emission_image, "{}_emission.tga".format(name))
 
     linear_emission = [1.000, 0.553, 0.072]
     srgb_emission = [int(pow(x, 1 / 2.2) * 255) for x in linear_emission]
