385e3b6bdf0a18b15173c34c105fd4481a41e524
zauonlok <zauonlok@gmail.com>
2019-12-24 18:26:06 +0800

    Decrease LINE_SIZE and PATH_SIZE

--- renderer/core/macro.h
+++ renderer/core/macro.h
@@ -7,8 +7,8 @@
 #define TO_RADIANS(degrees) ((PI / 180) * (degrees))
 #define TO_DEGREES(radians) ((180 / PI) * (radians))
 
-#define LINE_SIZE 1024
-#define PATH_SIZE 2048
+#define LINE_SIZE 256
+#define PATH_SIZE 256
 
 #define UNUSED_VAR(x) ((void)(x))
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
