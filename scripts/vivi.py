b3a6c84b47b49b3c40d0cf0adf015e8c84d43164
zauonlok <zauonlok@gmail.com>
2020-04-02 23:44:01 +0800

    Add a new model

---
+++ scripts/vivi.py
@@ -0,0 +1,71 @@
+"""Preprocess the Vivi Ornitier model
+
+The model is available for download from
+    https://sketchfab.com/models/be1e505ef3304343bf00736644730c70
+
+The Python Imaging Library is required
+    pip install pillow
+"""
+
+from __future__ import print_function
+
+import json
+import os
+import zipfile
+
+from PIL import Image
+
+from utils.gltf import dump_obj_data
+
+SRC_FILENAME = "vivi_ornitier_-_retrogasm_art_competition.zip"
+DST_DIRECTORY = "../assets/vivi"
+
+IMG_FILENAMES = {
+    "textures/lambert14SG_baseColor.png": "vivi_diffuse.tga",
+    "textures/lambert16SG_baseColor.png": "floor_diffuse.tga",
+}
+
+
+def process_meshes(zip_file):
+    gltf = json.loads(zip_file.read("scene.gltf"))
+    buffer = zip_file.read("scene.bin")
+
+    for mesh_index in range(len(gltf["meshes"])):
+        obj_data = dump_obj_data(gltf, buffer, mesh_index)
+        filename = "vivi{}.obj".format(mesh_index)
+        filepath = os.path.join(DST_DIRECTORY, filename)
+        with open(filepath, "w") as f:
+            f.write(obj_data)
+
+
+def load_image(zip_file, filename):
+    with zip_file.open(filename) as f:
+        image = Image.open(f)
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+        return image
+
+
+def save_image(image, filename, size=512):
+    if max(image.size) > size:
+        image = image.resize((size, size), Image.LANCZOS)
+    filepath = os.path.join(DST_DIRECTORY, filename)
+    image.save(filepath, rle=True)
+
+
+def process_images(zip_file):
+    for old_filename, tga_filename in IMG_FILENAMES.items():
+        image = load_image(zip_file, old_filename)
+        save_image(image, tga_filename)
+
+
+def main():
+    if not os.path.exists(DST_DIRECTORY):
+        os.makedirs(DST_DIRECTORY)
+
+    with zipfile.ZipFile(SRC_FILENAME) as zip_file:
+        process_meshes(zip_file)
+        process_images(zip_file)
+
+
+if __name__ == "__main__":
+    main()
